cd088f54a6ed77257da3b6ca3a70885db5a2410f
erw79 <99843247+erw79@users.noreply.github.com>
2024-11-10 19:46:26 +0000

    last session

--- 20questions.py
+++ 20questions.py
@@ -1,9 +1,10 @@
 
 # Print an introductory message
+print("Hello! Welcome to 20 questions. Think of an animal and we will guess the animal in less than 20 questions!")
 
 
 # Ask the user for an input to our first question (Whatever you want)
-
+user_input = input("Is it an animal? ")
 
 # Display the stored input
-
+print(user_input)

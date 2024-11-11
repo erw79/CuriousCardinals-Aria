1c591b931a390f3624f3e2e09d81e42e73947633
erw79 <99843247+erw79@users.noreply.github.com>
2024-11-11 23:11:46 +0000

    ready

---
+++ 11-11.py
@@ -0,0 +1,44 @@
+# ************** PRACTICE 1 *********************
+# Play around with Data types by printing the type() function fo the following elements 
+# Make a guess outloud before printing the output.
+
+
+#1) "Hello"
+
+#2) 5
+
+#3) 5.0
+
+#4) "5.0"
+
+#5) "True"
+
+#6) True
+
+#7) ["apple","orange","banana"]
+
+# ************** PRACTICE 2 *********************
+# Play around with Data types by printing the type() function fo the following operations 
+# Make a guess outloud before printing the output.
+
+#1) 1 + 3
+
+#2) 4/3
+
+#3) 4.0/3.0
+
+#4) "Hello" + "What's Up?"
+
+#5) Fix the formating of the last question
+
+#6) 3>4 
+
+#7) 5!=5 
+
+#8) 5=5
+
+#9) True and False
+
+#10) True or False
+
+#11) 3!=5 and 5==5
--- 20questions.py
+++ 20questions.py
@@ -1,10 +1,24 @@
+#List of questions
+questions = [
+    "Does it Have Fur?", 
+    "Can it Fly?", 
+    "Is it a Mammal?", 
+    "Does it Lays Eggs?",
+]
+
+
+
 
 # Print an introductory message
 print("Hello! Welcome to 20 questions. Think of an animal and we will guess the animal in less than 20 questions!")
 
+# Make a list of possibile answers and place 3 animals inside. Store this in a variable named possible_answers
 
-# Ask the user for an input to our first question (Whatever you want)
-user_input = input("Is it an animal? ")
 
-# Display the stored input
-print(user_input)
+
+# Ask the user for an input to a random first question and store it into a variable (like last week)
+# Put this input statement into a For Loop
+# You should loop through each question and place it into an input statement
+# You will have to replace the random question we made up in the step above for this part. 
+
+

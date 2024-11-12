608646095bbbbe09991d02458652ccf4440e448e
as4222 <ariaschorr@gmail.com>
2024-11-12 03:01:51 +0000

    Data type practice

--- 11-11.py
+++ 11-11.py
@@ -4,31 +4,35 @@
 
 
 #1) "Hello"
-
+print(type("Hello")) 
 #2) 5
-
+print(type(5))
 #3) 5.0
-
+print(type(5.0))
 #4) "5.0"
-
+print(type("5.0"))
 #5) "True"
-
+print(type("True"))
 #6) True
-
+print(type(True))
 #7) ["apple","orange","banana"]
-
+print(type(["apple","orange","banana"]))
 # ************** PRACTICE 2 *********************
 # Play around with Data types by printing the type() function fo the following operations 
 # Make a guess outloud before printing the output.
 
 #1) 1 + 3
-
+print(1+3)
+print ("Integer")
 #2) 4/3
-
+print (4/3)
+print ("float")
 #3) 4.0/3.0
-
+print (4.0/3.0)
+print ("float")
 #4) "Hello" + "What's Up?"
-
+print ("Hello" + " What's Up?")
+print ("string")
 #5) Fix the formating of the last question
 
 #6) 3>4 

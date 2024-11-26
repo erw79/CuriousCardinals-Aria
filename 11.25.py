d37bca0dbc2eea7e08731b60217fd1e8053fec0a
erw79 <99843247+erw79@users.noreply.github.com>
2024-11-26 01:05:48 +0000

    today

---
+++ 11.25.py
@@ -0,0 +1,6 @@
+## Define and call a function that Takes in a temperature in Fahrenheit and converts it into Celsius
+
+##Define the Function here
+
+##Call the function for 45 degrees F and 32 degrees F 
+## Print the results in this form: "50 Degrees F is: 10 degrees C"

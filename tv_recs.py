cc0ee9408016c2192e532fd4c1f286e180901df7
erw79 <99843247+erw79@users.noreply.github.com>
2024-11-29 23:10:20 +0000

    activiy

---
+++ tv_recs.py
@@ -0,0 +1,10 @@
+##Write a function that takes in a parameter - question (a string with the question we want to ask inside)
+## The function should ask the user the question and store the answer in a variable.
+## Then, using if/else statements, make sure that the user has entered a valid answer.
+## If the answer is invalid, tell the user and return nothing.
+
+def ask_question(question):
+    ## Start code here
+
+## After writing the function, call it with our program's first question as the argument.
+## Store the answer in a variable.

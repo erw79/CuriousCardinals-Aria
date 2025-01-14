a71b80cabd7e9b4cc75825ac080ae5959d24955b
as4222 <ariaschorr@gmail.com>
2025-01-14 03:03:02 +0000

    1/13/25

--- tv_recs.py
+++ tv_recs.py
@@ -1,11 +1,21 @@
 # Asks the question and checks if the answer is valid.
 def ask_question(question):
-    answer = input(question)
-    if answer == "a" or answer == "b" or answer == "c":
-        return answer
-    else:
-        print ("invalid answer")
-        return
+   corect_answer = True
+   while corect_answer:
+        answer = input(question)
+        if answer == "a" or answer == "b" or answer == "c":
+            corect_answer = False
+      
+        else:
+            print ("invalid answer")
+      
+    return answer 
+d = {}
+d ["aaa"] = "Nobody Wants This"
+d ["aba"] = "The White Lotus"
+d ["aca"] = "How to Die Alone"
+d ["aab"] = "Emily in Paris"
+d ["aac"] = "Brooklyn Nine-Nine"
 
 
  #print instructions
@@ -21,82 +31,4 @@ answer2 = ask_question ("Where do you prefer to stream TV?")
 #Ask third question about the number of seasons and store the answer.
 answer3 = ask_question ("How many seasons?")
 
-# If AAA choosen 
-
-
-# If AAB choosen
-
-
-#  if AAC choosen 
-
-
-# if ABA choosen
-
-
-# if ABB choosen
-
-
-# if ABC choosen
-
-
-# If ACA choosen
-
-
-# If ACB choosen
-
-
-#If ACC choosen
-
-
-#If BAA choosen
-
-
-# if BAB choosen
-
-
-# If BAC choosen
-
-
-# If BBA choosen
-
-
-#If BBB choosen
-
-
-#If BBC choosen
-
-
-#If BCA choosen
-
-
-#If BCB choosen
-
-
-#If BCC choosen
-
-
-# If CAA choosen
-
-
-#If CAB choosen
-
-
-# If CAC choosen
-
-
-#If CBA choosen
-
-
-#If CBB choosen
-
-
-#If CBC choosen
-
-
-# If CCA choosen
-
-
-# If CCB choosen
-
-
-## If CCC choosen
+print (d[answer1 + answer2 + answer3])

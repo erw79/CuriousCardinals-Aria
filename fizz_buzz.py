aac98cf276ff1c8c4ffcf6485b21c65ea0e9a939
erw79 <99843247+erw79@users.noreply.github.com>
2025-03-11 01:30:00 +0000

    fizz buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,42 @@
+## Division Practice
+## Fill in what you think the output of the following operations will be
+
+# Float Division
+print(7 / 2)   
+print("My Guess: ")
+
+print(10 / 4)  
+print("My Guess: ")
+
+print(10 / 2)  
+print("My Guess: ")
+
+
+# Integer Division
+print(7 // 2)  
+print("My Guess: ")
+
+print(10 // 4)
+print("My Guess: ")
+
+print(10 // 2)  
+print("My Guess: ")
+
+# Modulus
+print(7 / 2)   
+print("My Guess: ")
+
+print(10 % 2)  
+print("My Guess: ")
+
+
+
+## Write a program that prints the numbers 1-50
+## But...
+#  If the number is divisible by 3 print "Fizz" instead
+# If the number is divisible by 5 print "Buzz" instead
+# If the number is divisible by both 5 and 3 print "FizzBuzz"
+# If neither just print the number. 
+# Utilize loops, if statements etc. !!
+
+

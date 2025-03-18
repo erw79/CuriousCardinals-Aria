fb597b72f2bd1cc4600c2ef4ec88bf68d6895551
erw79 <99843247+erw79@users.noreply.github.com>
2025-03-18 01:30:49 +0000

    debug

---
+++ debug.py
@@ -0,0 +1,16 @@
+##Run the following code: 
+## Debug if there are any issues
+def greet(name)
+    print("Hello, " + name)
+
+students = ["Alice", "Bob"]
+print("Third student is:", students[2])  
+
+def calculate_average(numbers):
+    total = sum(numbers)
+    count = len(numbers)
+    return total / count  
+
+scores = []
+average = calculate_average(scores)
+print("The average score is:", average)

674d24c02e0d9db454e9ff1c84162d9a1da13058
as4222 <ariaschorr@gmail.com>
2025-03-18 01:25:17 +0000

    3/17/25

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,31 +3,31 @@
 
 # Float Division
 print(7 / 2)   
-print("My Guess: ")
+print("My Guess: 3.5")
 
 print(10 / 4)  
-print("My Guess: ")
+print("My Guess: 2.5")
 
 print(10 / 2)  
-print("My Guess: ")
+print("My Guess: 5.0")
 
 
 # Integer Division
 print(7 // 2)  
-print("My Guess: ")
+print("My Guess: 3")
 
 print(10 // 4)
-print("My Guess: ")
+print("My Guess: 2")
 
 print(10 // 2)  
-print("My Guess: ")
+print("My Guess: 5")
 
 # Modulus
-print(7 / 2)   
-print("My Guess: ")
+print(7 % 2)   
+print("My Guess: 1")
 
 print(10 % 2)  
-print("My Guess: ")
+print("My Guess: 0")
 
 
 
@@ -38,5 +38,14 @@ print("My Guess: ")
 # If the number is divisible by both 5 and 3 print "FizzBuzz"
 # If neither just print the number. 
 # Utilize loops, if statements etc. !!
-
-
+number = 0
+for i in range(50):
+    number + 1 
+    if number % 3 == 0:
+        print("Fizz")
+    if number % 5 == 0:
+        print ("Buzz")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    if number % 3 > 0 and number % 5 > 0:
+        print (number)

dffc3746e5021bee6e9cc9e5fc232c9cd544871d
erw79 <99843247+erw79@users.noreply.github.com>
2025-03-25 01:37:27 +0000

    list practice

---
+++ list_practice.py
@@ -0,0 +1,24 @@
+# Step 1: Create a Grocery List and print the first and last element
+
+
+
+
+
+# Step 2: Adding and Removing Items
+# Add bananas
+# Add yogurt
+# Remove bread
+print("Updated Grocery List:", grocery_list)
+
+# Step 3: Finding and Updating Items
+# Find index of milk
+# Replace with almond milk
+
+
+print("After Replacement:", grocery_list)
+
+# Step 4: Sorting and Counting Items
+grocery_list.sort()  # Sort list alphabetically
+apple_count = grocery_list.count("apples")  # Count apples
+print("Sorted Grocery List:", grocery_list)
+print("Number of apples in stock:", apple_count)

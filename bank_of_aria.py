a6fc0d418def9e06b13b0557408bf6f5a6c5e0a8
as4222 <ariaschorr@gmail.com>
2025-05-13 02:20:03 +0000

    5/12/25

--- bank_of_aria.py
+++ bank_of_aria.py
@@ -39,13 +39,10 @@ accounts = [
     BankAccount("Sam", 1900, "savings"),
     BankAccount("Tina", 2000, "checking"),
    
-
-# Code Starts Here
-    BankAccount("Alice", 100, "checking"),
-    BankAccount("Bob", 200, "savings"),
-    BankAccount("Charlie", 300, "savings"),
-    BankAccount("Charlie", 100, "checking")
 ]
+# Code Starts Here
+   
+
 bob_saving = BankAccount("Bob", 200, "savings")
 charlie_saving = BankAccount("Charlie", 300, "savings")
 alice_checking = BankAccount("Alice", 100, "checking")
@@ -54,10 +51,26 @@ bob_saving.deposit(200)
 charlie_saving.deposit(300)
 alice_checking.withdraw(50)
 charlie_checking.withdraw(80)
+accounts.append(bob_saving)
+accounts.append(charlie_saving)
+accounts.append(charlie_checking)
+accounts.append(alice_checking)
 
-# for acc in accounts:
-if bob_saving.account_type == "savings":
-    bob_balance =(105/100) * bob_saving.balance 
-    bob_saving.balance =  bob_balance
-    print (bob_balance)
+for acc in accounts:
+    if acc.account_type == "savings":
+        new_amount =(105/100) * acc.balance 
+        acc.balance = new_amount
+        print (new_amount)
+    print (acc.owner,acc.account_type,acc.balance)
 
+for acc in accounts:
+    savings = 0
+    checkings = 0
+    if acc.account_type == "savings":
+        savings = savings + 1
+        print("works")
+    if acc.account_type == "checking":
+        checkings = checkings + 1
+    
+print (savings)
+print ("There are",checkings,"checkings accounts")
